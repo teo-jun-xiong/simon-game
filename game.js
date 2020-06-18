9ba75e2f9a427e61b7d1f324ed2aa5f12c4d8e79
waaaflesss <50788391+waaaflesss@users.noreply.github.com>
2020-06-18 20:41:50 +0800

    Initial commit

---
+++ game.js
@@ -0,0 +1,82 @@
+var hasStarted = false;
+var level = 0;
+
+var gamePattern = [];
+var userClickedPattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+// Starts the game when a button is pressed
+$(document).keypress(function(event) {
+    if (!hasStarted) {
+        hasStarted = true;
+        nextSequence();
+    }
+});
+
+// Registers user button clicks as answers
+$(".btn").click(function(event) {
+    if (hasStarted) {
+        var userChosenColor = event.target.id;
+        userClickedPattern.push(userChosenColor);
+
+        playSound(userChosenColor);
+        animatePress(userChosenColor);
+
+        if (gamePattern.length === userClickedPattern.length) {
+            checkAnswer();
+        }
+    }
+});
+
+// Shows the next button
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    playSound(randomChosenColor);
+    animatePress(randomChosenColor);
+}
+
+function playSound(color) {
+    var audio = new Audio("sounds/" + color + ".mp3");
+    audio.play();
+}
+
+function animatePress(color) {
+    $("#" + color).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + color).addClass("pressed");
+    setTimeout(function() {
+        $("#" + color).removeClass("pressed");
+    }, 100);
+}
+
+function levelUp() {
+    $("h1").text("Level " + level++);
+    console.log("success");
+}
+
+function checkAnswer() {
+    // If answer is correct, then level up and show the next button
+    if (JSON.stringify(gamePattern) === JSON.stringify(userClickedPattern)) {
+        levelUp();
+        userClickedPattern = [];
+        setTimeout(nextSequence, 1000);
+    } else {
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        restartGame();
+        console.log("fail");
+    }
+}
+
+function restartGame() {
+    gamePattern = [];
+    userClickedPattern = [];
+    level = 0;
+    hasStarted = false;
+    $("h1").text("Game Over, Press Any Key to Restart");
+}
